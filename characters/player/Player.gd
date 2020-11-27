688d8f7f42bca1459786100553697b0264fa8266
Steven Tuckness <stuckne1@gmail.com>
2020-11-27 12:06:45 -0600

    Section 2: Weapon Manager

--- characters/player/Player.gd
+++ characters/player/Player.gd
@@ -1,10 +1,24 @@
 extends KinematicBody
 
+var hotkeys = {
+	KEY_1: 0,
+	KEY_2: 1,
+	KEY_3: 2,
+	KEY_4: 3,
+	KEY_5: 4,
+	KEY_6: 5,
+	KEY_7: 6,
+	KEY_8: 7,
+	KEY_9: 8,
+	KEY_0: 9	
+}
+
 export var mouse_sens = 0.5
 
 onready var camera = $Camera
 onready var character_mover = $CharacterMover
 onready var health_manager = $HealthManager
+onready var weapon_manager = $Camera/WeaponManager
 
 var dead = false
 
@@ -39,7 +53,15 @@ func _input(event):
 		rotation_degrees.y -= mouse_sens * event.relative.x
 		camera.rotation_degrees.x -= mouse_sens * event.relative.y
 		camera.rotation_degrees.x = clamp(camera.rotation_degrees.x, -90, 90)
-		
+	if event is InputEventKey and event.pressed:
+		if event.scancode in hotkeys:
+			weapon_manager.switch_to_weapon_slot(hotkeys[event.scancode])
+	if event is InputEventMouseButton and event.pressed:
+		if event.button_index == BUTTON_WHEEL_DOWN:
+			weapon_manager.switch_to_next_weapon()
+		else:
+			weapon_manager.switch_to_last_weapon()
+
 func hurt(damage, dir):
 	health_manager.hurt(damage, dir)
 	
