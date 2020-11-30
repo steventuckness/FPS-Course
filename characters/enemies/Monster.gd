9e2adf8ebd4c1b0f51399f21143f9458c60e6314
Steven Tuckness <stuckne1@gmail.com>
2020-11-29 22:39:34 -0600

    Section 4: Setting Up Enemy Melee Attacks

--- characters/enemies/Monster.gd
+++ characters/enemies/Monster.gd
@@ -4,6 +4,7 @@ onready var character_mover = $CharacterMover
 onready var anim_player = $Graphics/AnimationPlayer
 onready var health_manager = $HealthManager
 onready var nav: Navigation = get_parent()
+onready var aimer = $AimAtObject
 
 enum STATES { IDLE, CHASE, ATTACK, DEAD }
 var cur_state = STATES.IDLE
@@ -12,10 +13,23 @@ var player = null
 var path = []
 
 export var sight_angle = 45.0
-
 export var turn_speed = 360
 
+export var attack_range = 20
+export var attack_rate = 0.5
+export var attack_anim_speed_mod = 0.5
+var attack_timer: Timer
+var can_attack = true
+
+signal attack
+
 func _ready():
+	attack_timer = Timer.new()
+	attack_timer.wait_time = attack_rate
+	attack_timer.connect("timeout", self, "finish_attack")
+	attack_timer.one_shot = true
+	add_child(attack_timer)
+	
 	player = get_tree().get_nodes_in_group("player")[0]
 	var bone_attachments = $Graphics/Armature/Skeleton.get_children()
 	for bone_attachment in bone_attachments:
@@ -35,6 +49,8 @@ func _process(delta):
 			process_state_chase(delta)
 		STATES.DEAD:
 			process_state_dead(delta)
+		STATES.ATTACK:
+			process_state_attack(delta)
 
 func set_state_idle():
 	cur_state = STATES.IDLE
@@ -43,6 +59,10 @@ func set_state_idle():
 func set_state_chase():
 	cur_state = STATES.CHASE
 	anim_player.play("walk_loop", 0.2)
+	
+func set_state_attack():
+	cur_state = STATES.ATTACK
+	#anim_player.play("attack")
 
 func set_state_dead():
 	cur_state = STATES.DEAD
@@ -55,6 +75,8 @@ func process_state_idle(delta):
 		set_state_chase()
 	
 func process_state_chase(delta):
+	if within_dis_of_player(attack_range) and has_los_player():
+		set_state_attack()
 	var player_pos = player.global_transform.origin
 	var our_pos = global_transform.origin
 	path = nav.get_simple_path(our_pos, player_pos)
@@ -67,7 +89,13 @@ func process_state_chase(delta):
 	face_dir(dir, delta) 
 	
 func process_state_attack(delta):
-	pass
+	character_mover.set_move_vec(Vector3.ZERO)
+	# face_dir(global_transform.origin.direction_to(player.global_transform.origin), delta)
+	if can_attack:
+		if !within_dis_of_player(attack_range) or !can_see_player():
+			set_state_chase()
+		else:
+			start_attack()
 	
 func process_state_dead(delta):
 	pass
@@ -76,6 +104,18 @@ func hurt(damage: int, dir: Vector3):
 	if cur_state == STATES.IDLE:
 		set_state_chase()
 	health_manager.hurt(damage, dir)
+	
+func start_attack():
+	can_attack = false
+	anim_player.play("attack", -1, attack_anim_speed_mod)
+	attack_timer.start()
+	aimer.aim_at_pos(player.global_transform.origin + Vector3.UP)
+	
+func emit_attack_signal():
+	emit_signal("attack")
+	
+func finish_attack():
+	can_attack = true
 	
 func can_see_player():
 	var dir_to_player = global_transform.origin.direction_to(player.global_transform.origin)
@@ -107,7 +147,9 @@ func alert(check_los=true):
 		return
 	set_state_chase() 
 	
-	
+func within_dis_of_player(dis: float):
+	return global_transform.origin.distance_to(player.global_transform.origin) < attack_range
+			
 	
 	
 	
--- weapons/DamageArea.gd
+++ weapons/DamageArea.gd
@@ -11,6 +11,7 @@ func set_bodies_to_exclude(_bodies_to_exclude: Array):
 	bodies_to_exclude = _bodies_to_exclude
 
 func fire():
+	print("attack")
 	for body in get_overlapping_bodies():
 		if body.has_method("hurt") and !body in bodies_to_exclude:
 			body.hurt(damage, global_transform.origin.direction_to(body.global_transform.origin))
